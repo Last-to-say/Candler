30fb550b51666e0cec7c30a789eccdaf996e1108
Last-to-say <stanislavpatlakha@gmail.com>
2023-07-16 11:16:48 +0200

    added hrader and footer

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@ function watching() {
       baseDir: 'app/'
     }
   }); 
-  watch(['app/scss/style.scss'], styles)
+  watch(['app/scss/**/*.scss'], styles)
   watch(['app/images/src'], images)
   watch(['app/js/main.js'], scripts)
   watch(['app/pages/**/*.html'], htmlInclude);
